a065afd736c7db5378a03bab39542934b620ca7d
Zac Waite <129032699zw@gmail.com>
2023-06-04 14:29:45 -0400

    HTTP, Cookie, Language modules

---
+++ src/modules/language.ts
@@ -0,0 +1,38 @@
+import { LanguageType, languages } from "../types/languages"
+import { Cookies } from "./cookies"
+
+export const languageMap: {[K in LanguageType]: string} = {
+	Chinese: 'zh-cn',
+	English: 'en',
+	Afrikaans: 'af',
+	Armenian: 'hy',
+	Portuguese: 'pt',
+	Urdu: 'ur',
+	Spanish: 'es',
+	French: 'fr',
+	Arabic: 'ar',
+	Polish: 'pl',
+	Filipino: 'tl',
+	Croatian: 'hr',
+	Serbian: 'sr',
+	Romanian: 'ro',
+	German: 'de',
+	Italian: 'it',
+	Dutch: 'nl',
+	Hindi: 'hi',
+	Korean: 'ko',
+	Japanese: 'ja',
+	Persian: 'fa',
+	Punjabi: 'pa',
+	Gujarati: 'gu',
+	Turkish: 'tr',
+	Thai: 'th',
+	Russian: 'ru',
+	Amharic: 'am',
+} as const
+
+export const getLanguage = (): LanguageType | undefined => {
+	const language = Cookies.get('swril-language')
+	return (language && languages.includes(language as LanguageType)) ? language as LanguageType : undefined
+}
+
---
+++ src/modules/postalCode.ts
@@ -0,0 +1,11 @@
+import { Cookies } from "./cookies"
+
+export const validatePostalCode = (postalCode: string): boolean => {
+	const regex = /^[a-zA-Z]\d[a-zA-Z]$/
+	return regex.test(postalCode)
+}
+
+export const getPostalCode = (): string | undefined => {
+	const postalCode = Cookies.get('swril-postal-code')
+	return (postalCode && validatePostalCode(postalCode)) ? postalCode : undefined
+}
