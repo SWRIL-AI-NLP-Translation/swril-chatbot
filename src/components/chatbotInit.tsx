0936b745140ea941d416bc21bd641ad055bc09b8
Zac Waite <129032699zw@gmail.com>
2023-06-11 23:50:01 -0400

    Change init text

--- src/components/chatbotInit.tsx
+++ src/components/chatbotInit.tsx
@@ -15,7 +15,7 @@ export const ChatbotInit = (): JSX.Element => {
 
 	return (
 		<div>
-			<h3>To use this chatbot, enter your postal code:</h3>
+			<h3>To use this chatbot, enter the first 3 digits of your postal code:</h3>
 			<input type='text' value={newPostalCode} onChange={(npc) => setNewPostalCode(npc.target.value)} />
 			<button onClick={submitPostalCode}>Submit</button>
 		</div>
