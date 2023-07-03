d87beca7c180410a81841d2af2f01add4efe1d50
Zac Waite <129032699zw@gmail.com>
2023-07-03 14:38:41 -0400

    Integrate post feedback with backend

--- src/components/feedbackModal.tsx
+++ src/components/feedbackModal.tsx
@@ -3,6 +3,8 @@ import { FeedbackType } from '../types/feedback'
 import { GenericModal } from './genericModal'
 import { SWRILp } from './text'
 import { DataContext } from '../contexts/data'
+import { sendFeedback } from '../data/queries/feedbackResponse'
+import { ChatResponseType } from '../types/chatResponse'
 
 export const FeedbackModal = ({ feedback, close }: {
 	feedback: FeedbackType,
@@ -10,19 +12,40 @@ export const FeedbackModal = ({ feedback, close }: {
 }): JSX.Element => {
 	const { addData, getData } = React.useContext(DataContext)
 	const emoji = feedback.approve ? '👍' : '👎'
-	const trySendFeedback = (): void => {
+	const trySendFeedback = async (): Promise<void> => {
 		const messages = getData().messages
 		const messageIndex = messages.findIndex((message) => {
 			return message.id === feedback.message.id
 		})
 		if (messageIndex === -1) throw new Error('Message not found')
+		let prompt = ''
+		for (let i=messageIndex; i>0; i--) {
+			const userMsg = messages[i]
+			if (!userMsg.bot) {
+				prompt = userMsg.text ?? ''
+				break
+			}
+		}
+		const botMessage = messages[messageIndex]
+		const response: ChatResponseType = {
+			message: botMessage.text,
+			service: botMessage.service,
+			id: botMessage.id
+		}
+
+		const res = await sendFeedback(response, prompt, feedback.approve)
+		if (res.error) {
+			alert(res.error.message)
+			return
+		}
+		alert('Feedback sent')
+
 		const newMessages = [...messages]
 		newMessages[messageIndex] = {
 			...feedback.message,
 			positiveFeedback: feedback.approve
 		}
 		addData({ messages: newMessages })
-		alert('Feedback sent!')
 		close()
 	}
 	return (
--- src/data/queries/feedbackResponse.ts
+++ src/data/queries/feedbackResponse.ts
@@ -1,7 +1,38 @@
+import { ChatResponseType } from '../../types/chatResponse'
+import { DataResponse, DataResponseType } from '../../types/data'
+import { graphql } from '../graphql'
+
 export const FEEDBACK_RESPONSE = `
-	query GetFeedbackResponse($data: FeedbackInput!) {
-		getFeedbackResponse(data: $data) {
-            response
-		}
+	mutation PostFeedback($data: FeedbackInput!) {
+		postFeedback(data: $data)
 	}
 `
+
+
+export const sendFeedback = async (
+	response: ChatResponseType,
+	prompt: string,
+	positive: boolean,
+): Promise<DataResponseType<null>> => {
+
+	try {
+		const res = await graphql.request<{ data: { postFeedback: boolean }}>(FEEDBACK_RESPONSE, {
+			data: {
+				response,
+				prompt,
+				positive,
+				model: 'MONKEY'
+			},
+		})
+		if (res.error) {
+			return res
+		}
+		if (!res.data.data.postFeedback) {
+			return DataResponse.fatal('Message too old to post feedback')
+		}
+		return DataResponse.success(null)
+	} catch (e) {
+		console.error(e)
+		return DataResponse.fatal('Error')
+	}
+}
