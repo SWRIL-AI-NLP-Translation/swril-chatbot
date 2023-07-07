cf0b3583fc05a6dda98cc7d1294e78d3a0e60a05
Zac Waite <129032699zw@gmail.com>
2023-07-07 01:25:06 -0400

    Improve UI for chatbot Init

--- src/components/chatbotInit.tsx
+++ src/components/chatbotInit.tsx
@@ -1,6 +1,7 @@
 import { DataContext } from '../contexts/data'
 import React from 'react'
 import { validatePostalCode } from '../modules/postalCode'
+import { SWRILh2 } from './text'
 
 export const ChatbotInit = (): JSX.Element => {
 	const { getData, addData } = React.useContext(DataContext)
@@ -14,10 +15,18 @@ export const ChatbotInit = (): JSX.Element => {
 	}
 
 	return (
-		<div>
-			<h3>To use this chatbot, enter the first 3 digits of your postal code:</h3>
-			<input type='text' value={newPostalCode} onChange={(npc) => setNewPostalCode(npc.target.value)} />
-			<button onClick={submitPostalCode}>Submit</button>
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1rem' }}>
+			<SWRILh2>To use this chatbot, enter the first 3 digits of your postal code:</SWRILh2>
+			<div style={{ margin: '1rem' }}>
+				<input 
+					style={{ width: '4rem', textAlign: 'center' }} 
+					type='text' 
+					value={newPostalCode} 
+					onChange={(npc) => setNewPostalCode(npc.target.value)} 
+					placeholder='_ _ _'
+				/>
+				<button onClick={submitPostalCode}>Submit</button>
+			</div>
 		</div>
 	)
 }
